be80cb188db3f2d54af7a128f982dd4a3da53850
Asma Saud Omar <asmasauod3@gmail.com>
2025-07-08 19:22:36 +0300

    add Supervisor view

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
 using LabProject3.Data;
+using LabProject3.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<AppDbcontext>(options =>
+builder.Services.AddDbContext<Dbgroup2Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
